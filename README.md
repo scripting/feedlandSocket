1836766c9983326c8e321c5aad346ae7ffe88249
Dave Winer <dave.winer@gmail.com>
2022-11-22 17:31:16 -0500

    .

--- README.md
+++ README.md
@@ -1,2 +1,22 @@
 # feedlandSocket
-A demo app that shows you how to tap into FeedLand's websocket interface.
+
+A demo Node.js app that shows you how to tap into <a href="http://feedland.org/">FeedLand's</a> websocket interface.
+
+### Background
+
+While FeedLand is reading feeds, it's sending a constant stream of JavaScript objects to each user.
+
+This is how it updates feeds in realtime. 
+
+This service is also available to applications running in any environment that supports the W3C standard websockets.
+
+### How to use
+
+Download the repo, run <i>NPM install</i> in the folder and then run the app.
+
+`node feedlandsocket.js`
+
+Every time FeedLand finds a new item, you'll get a message over the websocket pipe with a payload that includes the item and information about the feed it came from. 
+
+The demo app  displays the title of the feed for every new item received, but there's a lot more info in the package that you can use however you like. 
+
---
+++ feedlandsocket.js
@@ -0,0 +1,51 @@
+const myVersion = "0.4.0", myProductName = "feedlandSocket"; 
+
+const fs = require ("fs");
+const utils = require ("daveutils");
+const websocket = require ('websocket').w3cwebsocket;
+
+var config = {
+	urlSocketServer: "ws://feedland.org:1466/" 
+	};
+
+function wsConnectUserToServer (itemReceivedCallback) {
+	var mySocket = undefined;
+	function checkConnection () {
+		if (mySocket === undefined) {
+			mySocket = new websocket (config.urlSocketServer); 
+			mySocket.onopen = function (evt) {
+				};
+			mySocket.onmessage = function (evt) {
+				function getPayload (jsontext) {
+					var thePayload = undefined;
+					try {
+						thePayload = JSON.parse (jsontext);
+						}
+					catch (err) {
+						}
+					return (thePayload);
+					}
+				if (evt.data !== undefined) { //no error
+					var theCommand = utils.stringNthField (evt.data, "\r", 1);
+					var jsontext = utils.stringDelete (evt.data, 1, theCommand.length + 1);
+					var thePayload = getPayload (jsontext);
+					switch (theCommand) {
+						case "newItem": 
+							itemReceivedCallback (thePayload);
+							break;
+						}
+					}
+				};
+			mySocket.onclose = function (evt) {
+				mySocket = undefined;
+				};
+			mySocket.onerror = function (evt) {
+				};
+			}
+		}
+	setInterval (checkConnection, 1000);
+	}
+
+wsConnectUserToServer (function (thePayload) {
+	console.log (new Date ().toLocaleTimeString () + ": " + thePayload.theFeed.title);
+	});
